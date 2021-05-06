867931fab4a2ea21b5a9b84a738b4dd27cc472b8
Essed automatisering <info@essed.com>
2021-05-06 15:09:51 +0200

    Problem: Filter.Clear not working as it should

--- TelerikWinFormsApp1 - Filter Problem/RadForm1.cs
+++ TelerikWinFormsApp1 - Filter Problem/RadForm1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Telerik.WinControls;
+using Telerik.WinControls.Data;
 using Telerik.WinControls.UI;
 
 namespace TelerikWinFormsApp1___Filter_Problem
@@ -16,6 +17,33 @@ namespace TelerikWinFormsApp1___Filter_Problem
         public RadForm1()
         {
             InitializeComponent();
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            // Enabling Excel-like filtering 
+            this.radGridView1.EnableFiltering = true;
+            this.radGridView1.MasterTemplate.ShowHeaderCellButtons = true;
+            this.radGridView1.MasterTemplate.ShowFilteringRow = false;
+
+            // Filling RadGridView
+            int i = 1;
+            radGridView1.DataSource = 
+                new List<double> { 12, 24, 500, 1000, 1000.34, 43,455, 55, 43, 55, 666, 10 }.Select(x => new
+                {
+                    Product = string.Format("Product {0}", i++),
+                    Amount = x
+                });
+
+            // Set FormatString to Dutch
+            radGridView1.Columns[1].FormatInfo = new System.Globalization.CultureInfo("nl-NL");
+            radGridView1.Columns[1].FormatString = "{0:€ #,###.00}";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radGridView1.FilterDescriptors.Clear();
         }
     }
 }
